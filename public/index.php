7ddbb2e350da7a2dca3446a4fdf7c52bee0398f5
Puxarnal <loic.cardin@gmail.com>
2021-06-23 00:11:10 +0200

    styling

--- public/index.php
+++ public/index.php
@@ -6,79 +6,88 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="icon" href="/img/favicon.png" />
+    <link rel="stylesheet" type="text/css" href="/build/css/main.css" />
 </head>
 <body>
-    <header>
-        <nav>
-            <a href="/" title="Revenir à l'accueil">
-                <img src="/img/logo.png" alt="Pokémory - Gotta find 'em all" />
-            </a>
-        </nav>
-    </header>
-    <main>
+    <div class="container">
+        <header class="mt-4 mb-3">
+            <nav>
+                <a href="/" title="Revenir à l'accueil">
+                    <img class="mw-100 h-auto" src="/img/logo.png" alt="Pokémory - Gotta find 'em all" />
+                </a>
+            </nav>
+        </header>
+        <main>
 
-        <a href="/play.php">
-            Jouer
-        </a>
+            <div class="d-grid d-md-block text-center">
+                <a class="btn btn-primary btn-lg p-3 px-md-5" href="/play.php">
+                    Jouer
+                </a>
+            </div>
 
-        <section>
-            <h2>
-                Meilleurs temps
-            </h2>
-            <ol>
-                <li>
-                    <b>
-                        Pseudo
-                    </b>
-                    -
-                    2 minutes
-                    36 secondes
-                </li>
-                <li>
-                    <b>
-                        Pseudo
-                    </b>
-                    -
-                    2 minutes
-                    36 secondes
-                </li>
-                <li>
-                    <b>
-                        Pseudo
-                    </b>
-                    -
-                    2 minutes
-                    36 secondes
-                </li>
-                <li>
-                    <b>
-                        Pseudo
-                    </b>
-                    -
-                    2 minutes
-                    36 secondes
-                </li>
-                <li>
-                    <b>
-                        Pseudo
-                    </b>
-                    -
-                    2 minutes
-                    36 secondes
-                </li>
-            </ol>
-        </section>
+            <section class="my-4">
+                <h2>
+                    Meilleurs temps
+                </h2>
+                <ol>
+                    <li>
+                        <b>
+                            Pseudo
+                        </b>
+                        -
+                        2 minutes
+                        36 secondes
+                    </li>
+                    <li>
+                        <b>
+                            Pseudo
+                        </b>
+                        -
+                        2 minutes
+                        36 secondes
+                    </li>
+                    <li>
+                        <b>
+                            Pseudo
+                        </b>
+                        -
+                        2 minutes
+                        36 secondes
+                    </li>
+                    <li>
+                        <b>
+                            Pseudo
+                        </b>
+                        -
+                        2 minutes
+                        36 secondes
+                    </li>
+                    <li>
+                        <b>
+                            Pseudo
+                        </b>
+                        -
+                        2 minutes
+                        36 secondes
+                    </li>
+                </ol>
+            </section>
 
-    </main>
-    <footer>
-        <nav>
-            <a href="#">
-                Voir sur GitHub
-            </a>
-            <a href="#">
-                Contacter l'auteur
-            </a>
-        </nav>
-    </footer>
+        </main>
+        <footer class="mt-5">
+            <nav class="row text-center">
+                <div class="col">
+                    <a href="#">
+                        Voir sur GitHub
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="#">
+                        Contacter l'auteur
+                    </a>
+                </div>
+            </nav>
+        </footer>
+    </div>
 </body>
 </html>
