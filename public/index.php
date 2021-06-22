1fe7f1dc525ac41da471238befe5f1398285cb0a
Pux <loic.cardin@gmail.com>
2021-06-22 15:56:07 +0200

    index mockup

---
+++ public/index.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Accueil | Pokémory</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" href="/img/favicon.png" />
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="/" title="Revenir à l'accueil">
+                <img src="/img/logo.png" alt="Pokémory - Gotta find 'em all" />
+            </a>
+        </nav>
+    </header>
+    <main>
+
+        <a href="/play.php">
+            Jouer
+        </a>
+
+        <section>
+            <h2>
+                Meilleurs temps
+            </h2>
+            <ol>
+                <li>
+                    <b>
+                        Pseudo
+                    </b>
+                    -
+                    2 minutes
+                    36 secondes
+                </li>
+                <li>
+                    <b>
+                        Pseudo
+                    </b>
+                    -
+                    2 minutes
+                    36 secondes
+                </li>
+                <li>
+                    <b>
+                        Pseudo
+                    </b>
+                    -
+                    2 minutes
+                    36 secondes
+                </li>
+                <li>
+                    <b>
+                        Pseudo
+                    </b>
+                    -
+                    2 minutes
+                    36 secondes
+                </li>
+                <li>
+                    <b>
+                        Pseudo
+                    </b>
+                    -
+                    2 minutes
+                    36 secondes
+                </li>
+            </ol>
+        </section>
+
+    </main>
+    <footer>
+        <nav>
+            <a href="#">
+                Voir sur GitHub
+            </a>
+            <a href="#">
+                Contacter l'auteur
+            </a>
+        </nav>
+    </footer>
+</body>
+</html>
