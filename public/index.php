45ce437ae1f5ba049df9e8ee41f71afe20eaadee
Pux <loic.cardin@gmail.com>
2021-06-23 13:08:04 +0200

    integration

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@
 <body>
     <div class="container">
         <header class="mt-4 mb-3">
-            <nav>
+            <nav class="text-center">
                 <a href="/" title="Revenir à l'accueil">
                     <img class="mw-100 h-auto" src="/img/logo.png" alt="Pokémory - Gotta find 'em all" />
                 </a>
@@ -21,59 +21,66 @@
         </header>
         <main>
 
-            <div class="d-grid d-md-block text-center">
-                <a class="btn btn-primary btn-lg p-3 px-md-5" href="/play.php">
-                    Jouer
-                </a>
+            <div class="row align-items-center my-4">
+                <div class="col-12 col-md">
+                    <div class="d-grid d-md-block text-center">
+                        <a class="btn btn-primary btn-lg p-3 px-md-5" href="/play.php">
+                            Jouer
+                        </a>
+                    </div>
+                </div>
+                <div class="col-12 col-md">
+                    <section class="mt-4 mt-md-0">
+                        <h2>
+                            Meilleurs temps
+                        </h2>
+                        <ol>
+                            <li>
+                                <b>
+                                    Pseudo
+                                </b>
+                                -
+                                2 minutes
+                                36 secondes
+                            </li>
+                            <li>
+                                <b>
+                                    Pseudo
+                                </b>
+                                -
+                                2 minutes
+                                36 secondes
+                            </li>
+                            <li>
+                                <b>
+                                    Pseudo
+                                </b>
+                                -
+                                2 minutes
+                                36 secondes
+                            </li>
+                            <li>
+                                <b>
+                                    Pseudo
+                                </b>
+                                -
+                                2 minutes
+                                36 secondes
+                            </li>
+                            <li>
+                                <b>
+                                    Pseudo
+                                </b>
+                                -
+                                2 minutes
+                                36 secondes
+                            </li>
+                        </ol>
+                    </section>
+                </div>
             </div>
 
-            <section class="my-4">
-                <h2>
-                    Meilleurs temps
-                </h2>
-                <ol>
-                    <li>
-                        <b>
-                            Pseudo
-                        </b>
-                        -
-                        2 minutes
-                        36 secondes
-                    </li>
-                    <li>
-                        <b>
-                            Pseudo
-                        </b>
-                        -
-                        2 minutes
-                        36 secondes
-                    </li>
-                    <li>
-                        <b>
-                            Pseudo
-                        </b>
-                        -
-                        2 minutes
-                        36 secondes
-                    </li>
-                    <li>
-                        <b>
-                            Pseudo
-                        </b>
-                        -
-                        2 minutes
-                        36 secondes
-                    </li>
-                    <li>
-                        <b>
-                            Pseudo
-                        </b>
-                        -
-                        2 minutes
-                        36 secondes
-                    </li>
-                </ol>
-            </section>
+
 
         </main>
         <footer class="mt-5">
