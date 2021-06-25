5884ecb4383e4a756736cc7162efa6549d731812
Puxarnal <loic.cardin@gmail.com>
2021-06-25 10:16:21 +0200

    doc

--- src/classes/Manager/ManagerInterface.php
+++ src/classes/Manager/ManagerInterface.php
@@ -2,9 +2,23 @@
 
 namespace Pokemory\Manager;
 
+/**
+ * Définit des méthodes que doivent implémenter les managers
+ * @see https://www.php.net/manual/fr/language.oop5.interfaces.php
+ */
 interface ManagerInterface
 {
+    /**
+     * Enregistre un modèle en base de données
+     * @param object $model
+     * @return bool
+     */
     public static function save($model): bool;
 
+    /**
+     * Supprime un modèle de la base de données
+     * @param object $model
+     * @return bool
+     */
     public static function delete($model): bool;
 }
